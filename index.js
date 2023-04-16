02de66e7e2cbf0620867274a8e0af2f2b8e7b89d
JaimeBaldin <73400106+JaimeBaldin@users.noreply.github.com>
2023-04-16 14:23:41 -0300

    Fazendo a primeira alteração no index utilizando o EJS, começo da página

---
+++ index.js
@@ -0,0 +1,13 @@
+import Express from "express";
+
+const app = Express();
+
+app.set("view engine","ejs");
+
+app.get("/",(req,res)=>{
+    res.render("index");
+});
+
+app.listen(8080,()=>{
+    console.log("Server is running on port 8080")
+});
